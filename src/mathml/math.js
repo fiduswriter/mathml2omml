cb83a902bb95631c46d2e1911a10d4fc285a7e60
kailunyao <yaokailun@cvte.com>
2024-02-19 14:16:54 +0800

    fix: Fix illegal w:rPr tag

--- src/mathml/math.js
+++ src/mathml/math.js
@@ -1,7 +1,8 @@
 export function math (element, targetParent, previousSibling, nextSibling, ancestors) {
   targetParent.name = 'm:oMath'
   targetParent.attribs = {
-    'xmlns:m': 'http://schemas.openxmlformats.org/officeDocument/2006/math'
+    'xmlns:m': 'http://schemas.openxmlformats.org/officeDocument/2006/math',
+    'xmlns:w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
   }
   targetParent.type = 'tag'
   targetParent.children = []
